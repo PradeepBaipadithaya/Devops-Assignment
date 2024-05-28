c4a454ae11835e78b97cb9e8848dab70ae597606
PradeepBaipadithaya <85048509+PradeepBaipadithaya@users.noreply.github.com>
2024-05-28 13:49:21 +0530

    Update UserController.java

--- src/main/java/com/project/controller/UserController.java
+++ src/main/java/com/project/controller/UserController.java
@@ -2,6 +2,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.dao.UserDao;
+
 @RestController
 public class UserController {
 
