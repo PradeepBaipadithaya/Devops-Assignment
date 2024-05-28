87b536121eecc7d4e7d5eb715c200cc251412dda
PradeepBaipadithaya <85048509+PradeepBaipadithaya@users.noreply.github.com>
2024-05-28 14:44:57 +0530

    Update UserDao.java

--- src/main/java/com/project/dao/UserDao.java
+++ src/main/java/com/project/dao/UserDao.java
@@ -1,17 +1,1 @@
-package com.project.dao;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class UserDao {
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    public int getUserCount() {
-        String sql = "SELECT COUNT(*) FROM users";
-        return jdbcTemplate.queryForObject(sql, Integer.class);
-    }
-}
