db8d91fcca592392c8db1c961af6afd7c72dfa04
PradeepBaipadithaya <85048509+PradeepBaipadithaya@users.noreply.github.com>
2024-05-28 12:18:44 +0530

    Update UserDao.java

--- src/main/java/com/project/dao/UserDao.java
+++ src/main/java/com/project/dao/UserDao.java
@@ -1,6 +1,7 @@
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+package com.project.dao;
 
 @Repository
 public class UserDao {
