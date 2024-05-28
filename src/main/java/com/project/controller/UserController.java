4e4c295aa8102049192bf9ffac8f54851cb31c64
PradeepBaipadithaya <85048509+PradeepBaipadithaya@users.noreply.github.com>
2024-05-28 12:08:17 +0530

    Create UserController.java

---
+++ src/main/java/com/project/controller/UserController.java
@@ -0,0 +1,16 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserDao userDao;
+
+    @GetMapping("/user/count")
+    public String getUserCount() {
+        int count = userDao.getUserCount();
+        return "Total number of users: " + count;
+    }
+}
