8e09731ef1d695576d2bb48b65a0373c13564487
PradeepBaipadithaya <85048509+PradeepBaipadithaya@users.noreply.github.com>
2024-05-28 14:45:19 +0530

    Update UserController.java

--- src/main/java/com/project/controller/UserController.java
+++ src/main/java/com/project/controller/UserController.java
@@ -1,18 +1,1 @@
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.project.dao.UserDao;
-
-@RestController
-public class UserController {
-
-    @Autowired
-    private UserDao userDao;
-
-    @GetMapping("/user/count")
-    public String getUserCount() {
-        int count = userDao.getUserCount();
-        return "Total number of users: " + count;
-    }
-}
