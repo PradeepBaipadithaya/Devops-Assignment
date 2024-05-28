915b2d584a8bf4556b0576be5e418ade892bd9d7
PradeepBaipadithaya <85048509+PradeepBaipadithaya@users.noreply.github.com>
2024-05-28 12:17:39 +0530

    Update LoginController.java

--- src/main/java/com/project/controller/LoginController.java
+++ src/main/java/com/project/controller/LoginController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import com.project.dao.LoginDao;
+import com.project.dao.UserDao;
+
 import com.project.dao.UsersInSystemDao;
 import com.project.dao.receptionist.PatientPrescriptionDao;
 import com.project.entity.Login;
